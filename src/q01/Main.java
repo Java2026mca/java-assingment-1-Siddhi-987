09858ad80cd36b81996bab06e274df7fc4965419
Siddhi-987 <siddhiy192@gmail.com>
2026-04-07 19:08:30 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,25 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class HollowRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
-
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
     }
 }
